027bc833aacad59b4fbfe0a12a29a5d0764a460e
hamza192 <41551916+hamza192@users.noreply.github.com>
2018-09-10 12:55:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -248,6 +248,39 @@ ${prefix}queue ⇏ لمعرفة قآئمة التشغيل
 
 
 
+client.on('message', message => {
+    if (message.content.startsWith("-avatar")) {
+if(!message.channel.guild) return;
+        var mentionned = message.mentions.users.first();
+    var client;
+      if(mentionned){
+          var client = mentionned; } else {
+          var client = message.author;
+      }
+        const embed = new Discord.RichEmbed()
+                           .addField('Requested by:', "<@" + message.author.id + ">")
+        .setColor(000000)
+        .setImage(`${client.avatarURL}`)
+      message.channel.sendEmbed(embed);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
